f67ba0cfca231386596d2a62858c3484010f17b8
K Castillo <krystalin.castillo@gmail.com>
2024-01-27 23:01:08 -0600

    User model created.

--- main_app/models.py
+++ main_app/models.py
@@ -10,6 +10,13 @@ class Icon(models.Model):
     description = models.TextField()
     debut_year = models.IntegerField()
 
+class User(models.Model):
+    # name = models.CharField(max_length=100)
+    # email = models.CharField(max_length=100)
+    # password = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    favorite = models.TextField()
+
 class User_Sneaker(models.Model):
     sneaker = models.CharField(max_length=100)
     sneaker_type = models.CharField(max_length=100)
